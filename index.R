6bde90cd3de64b5db4eafe13435c9b7b0b79f76c
William Simionatto <william.sn0226@gmail.com>
2022-08-23 19:40:48 -0300

    feat: identify missing values

--- index.R
+++ index.R
@@ -4,3 +4,8 @@ WineData <- read.table("winequality-red.csv", sep=",", header=TRUE)
 
 head(WineData)
 table(WineData$quality)
+
+install.packages("Amelia")
+
+library(Amelia)
+missmap(WineData, main="Valores Faltantes - Vinho Vermelho", col=c("red","grey"), legend=TRUE)
