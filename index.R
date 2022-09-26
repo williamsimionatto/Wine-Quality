0eb4018716659be861eef498f8e3b69f0f5735fa
William Simionatto <william.sn0226@gmail.com>
2022-09-26 19:38:13 -0300

    feat: adiciona variaveis categoricas para a qualidade do vinho

--- index.R
+++ index.R
@@ -5,12 +5,12 @@ WineData <- read.table("winequality-red.csv", sep=",", header=TRUE)
 head(WineData)
 table(WineData$quality)
 
-install.packages("Amelia")
+# install.packages("Amelia")
 
 library(Amelia)
 missmap(WineData, main="Valores Faltantes - Vinho Vermelho", col=c("red","grey"), legend=TRUE)
 
-install.packages("ggplot2")
+# install.packages("ggplot2")
 library(ggplot2)
 
 ggplot(WineData, aes(x = fixed.acidity)) +
@@ -26,11 +26,10 @@ ggplot(WineData, aes(x = pH)) +
   xlab("pH") +
   ylab("Count")
 
-
 ggplot(WineData, aes(x = free.sulfur.dioxide)) +
   geom_histogram(binwidth = 1) +
   ggtitle("Distribuição de SO2 liberado") +
-  xlab("Free SO2") +
+  xlab("SO2 Liberado") +
   ylab("Count")
 
 ggplot(WineData, aes(x = total.sulfur.dioxide)) +
@@ -42,7 +41,7 @@ ggplot(WineData, aes(x = total.sulfur.dioxide)) +
 ggplot(WineData, aes(x = alcohol)) +
   geom_histogram(binwidth = 0.1) +
   ggtitle("Distribuição do Alcool") +
-  xlab("Alcohol") +
+  xlab("Alcool") +
   ylab("Count")
 
 ggplot(WineData, aes(x = quality)) +
@@ -51,3 +50,22 @@ ggplot(WineData, aes(x = quality)) +
   ggtitle("Distribuição da Qualidade") +
   xlab("Qualidade") +
   ylab("Count")
+
+ggplot(WineData, aes(x = volatile.acidity)) +
+  geom_density(aes(fill = "red", color = "red")) +
+  facet_wrap(~quality) +
+  theme(legend.position = "none") +
+  ggtitle("Acidez Volátil VS Qualidade") +
+  xlab("Acidez Volátil") +
+  ylab("Qualidade")
+
+ggplot(WineData, aes(x = density, y = alcohol)) +
+  geom_jitter(aes(alpha = 1/2)) +
+  ggtitle("Densidade VS Alcool") +
+  xlab("Densidade") +
+  ylab("Alcool")
+
+# cria variáveis categoricas para a qualidade do vinho
+WineData$quality_descriotion <- ifelse(WineData$quality < 5, 'ruim', ifelse(WineData$quality > 6,'bom','normal'))
+WineData$quality_descriotion <- as.factor(WineData$quality_descriotion)
+str(WineData$quality_descriotion)
